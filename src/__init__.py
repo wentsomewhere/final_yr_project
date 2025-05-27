e53276c6855710644699990746abaf3319abf547
Wentsomewhere <srikargorty@gmail.com>
2025-05-27 14:04:10 +0530

    init change

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,9 @@
 """
-SRR-GAN: Super-Resolution based Recognition with GAN for Low-Resolved Text Images
+SRRGAN - Super Resolution GAN for Document Image Enhancement
 """
+
+from src.models import *
+from src.utils import *
+from src.data import *
 
 __version__ = "1.0.0" 
